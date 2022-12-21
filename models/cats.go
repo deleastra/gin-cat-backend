3e0b522cc5d98d3da263f9c8362f0a54d1d64575
Deleastra <blaz-iken@hotmail.com>
2022-12-21 16:09:22 +0700

    update cat and user models

--- models/cats.go
+++ models/cats.go
@@ -4,9 +4,10 @@ import (
 	"gorm.io/gorm"
 )
 
+// Cats represents a type of data object for storing information about cats.
 type Cats struct {
 	gorm.Model
-	ID    uint `json:"id" orm:"auto" gorm:"primary_key"`
-	Name  string
-	Image string
+	ID    uint   `gorm:"primary_key;auto_increment" json:"id"` // ID is the primary key and auto-incrementing field for the cats table in the database.
+	Name  string `json:"name"`                                 // Name is the name of the cat.
+	Image string `json:"image"`                                // Image is the file path or URL of an image for the cat.
 }
--- models/users.go
+++ models/users.go
@@ -5,9 +5,8 @@ import (
 )
 
 type Users struct {
-	gorm.Model
-	ID          uint   `json:"id" orm:"auto" gorm:"primary_key"`
-	Username    string `gorm:"unique;size:12"`
-	password    string
-	DisplayName string `gorm:"size:24"`
+	gorm.Model          // Embeds fields from the gorm.Model struct, including ID, CreatedAt, and UpdatedAt
+	Username    *string `gorm:"unique;size:12"` // Unique username with a size of 12 characters
+	Password    *string `json:"-"`              // Password for the user (not serialized as part of a JSON response)
+	DisplayName *string `gorm:"size:24"`        // Display name for the user with a size of 24 characters
 }
